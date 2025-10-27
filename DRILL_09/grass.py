679ea8dc2ebbb8b6bafb4273f72c0de53b6dfcc8
Jo shinBe <tls3123@tukorea.ac.kr>
2025-10-27 12:52:30 +0900

    x,y를 직접 입력받을 수 있게 수정(단차 위해서)

--- DRILL_09/grass.py
+++ DRILL_09/grass.py
@@ -2,11 +2,14 @@ from pico2d import load_image
 
 
 class Grass:
-    def __init__(self):
+    def __init__(self, x, y):
+        self.x= x
+        self.y =y
         self.image = load_image('grass.png')
 
     def draw(self):
-        self.image.draw(400, 30)
+        self.image.draw(self.x, self.y)
 
     def update(self):
         pass
+
